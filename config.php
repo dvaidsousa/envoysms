404b403aeea83340902fe04d2cba83adb5de651b
David Sousa <109256322+dvaidsousa@users.noreply.github.com>
2024-11-04 14:32:15 -0300

    Primeiro commit do projeto

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+define('DB_SERVER', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'sms_gateway');
+define('TIMEZONE', 'America/Sao_Paulo');
+define('APP_SECRET_KEY', 'u3E1lfK9B3J4wTJl7v4L5e7W');
+define('APP_SESSION_NAME', 'SMS_GATEWAY');
+define('PURCHASE_CODE', '74791a2d-bc85-44ad-ac41-tedd9aa43c10');
